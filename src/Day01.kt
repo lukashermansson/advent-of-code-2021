40a80999da4933cb82a2e6098f1cb0763a8845fa
Lukas Hermansson <lukas@lukashermansson.me>
2021-12-01 21:24:00 +0100

    Day one first half

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,17 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+    val valuesAsInts = input.map { it.toInt() }
+
+    var numberOfTimesIncreased = 0
+    var previusNumber = valuesAsInts.first()
+
+    valuesAsInts.drop(1).forEach {
+        if(it > previusNumber) {
+            numberOfTimesIncreased++
+        }
+        previusNumber = it
+    }
+
+    println("Numbers larger: ${numberOfTimesIncreased}")
 }
