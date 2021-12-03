9f8a87c81bb2bd0f0937b43765247f7ab2beb0e3
Lukas Hermansson <lukas@lukashermansson.me>
2021-12-03 14:53:44 +0100

    day three first half

---
+++ src/Day03.kt
@@ -0,0 +1,42 @@
+fun main() {
+    val input = readInput("Day03")
+
+    val map = input.map {
+        it.map {
+            it == '1'
+
+        }
+    }
+    var gammaString = ""
+    for (i in 0..11) {
+        gammaString+= if(getGammaBitForPosition(map, i)) '1' else "0"
+    }
+    var epsilonString = ""
+    for (i in 0..11) {
+        epsilonString+= if(getGammaBitForPosition(map, i)) '0' else "1"
+    }
+
+    println("Gamma string binary: ${gammaString}")
+    println("Elipsion string binary: ${epsilonString}")
+
+    val gamma = gammaString.toInt(2)
+    val epsilon = epsilonString.toInt(2)
+    println("Gamma value: ${gamma}")
+    println("Epsilon value: ${epsilon}")
+
+    println("Power of the submarine: ${ gamma*epsilon}")
+
+}
+
+fun getGammaBitForPosition(listy : List<List<Boolean>>, pos : Int) : Boolean{
+    var ones = 0
+    var zeros = 0
+    listy.forEach {
+        if(it.get(pos)){
+            ones++
+        }else {
+            zeros++
+        }
+    }
+    return ones > zeros
+}
